8fa8c9de7c171571b32ab593aa103fa5ac0b2665
Logan Koester <logan@logankoester.com>
2014-11-19 16:48:13 +0000

    Removes site-specific nginx config in favor of loading it from the deployed website repo

--- test/integration/default/serverspec/default/wordpress_spec.rb
+++ test/integration/default/serverspec/default/wordpress_spec.rb
@@ -1,6 +1,1 @@
 require 'spec_helper'
-
-describe file('/etc/nginx/sites/default.example.conf') do
-  its(:content) { should match /Add trailing slash.*wp-admin requests/ }
-  its(:content) { should match /WP Super Cache/ }
-end
