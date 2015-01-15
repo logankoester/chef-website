174657d274fc83b47f5bf60623192f558216091d
Logan Koester <logan@logankoester.com>
2015-01-15 01:20:31 +0000

    Read SSL object from data bag item into node attribute for ssl_certificate namespace

--- recipes/ssl.rb
+++ recipes/ssl.rb
@@ -6,6 +6,10 @@ sites.each do |site_id|
   site = data_bag_item 'sites', site_id
 
   if site['ssl']
+    # Set node attributes from data bag item ssl property
+    ns = "site_ssl_#{site_id}"
+    node.default[ns] = site['ssl']
+
     ['ssl', 'ssl/keys', 'ssl/certs'].each do |new_path|
       directory "#{site['root']}/#{new_path}" do
         owner site['owner']
@@ -15,11 +19,9 @@ sites.each do |site_id|
         recursive true
       end
     end
-  end
 
-  if site['ssl']
     ssl_certificate site_id do
-      namespace site
+      namespace node[ns]
       key_path File.join(site['root'], 'ssl', 'keys', "#{site_id}.key")
       cert_path File.join(site['root'], 'ssl', 'certs', "#{site_id}.pem")
       notifies :restart, 'service[nginx]' unless node['nginx']['supervisor']
--- test/integration/default/serverspec/default/ssl_spec.rb
+++ test/integration/default/serverspec/default/ssl_spec.rb
@@ -1,9 +1,22 @@
 require 'spec_helper'
 
+ssl_dir = '/srv/http/default/ssl'
+subject_command = "openssl x509 -noout -subject -in"
+fingerprint_command = "openssl x509 -noout -fingerprint -in"
+key_path = File.join(ssl_dir, 'keys', 'default.key')
+cert_path = File.join(ssl_dir, 'certs', 'default.pem')
+
+describe file('/etc/nginx/sites/default.conf') do
+  its(:content) { should match /listen 443;/ }
+  its(:content) { should match /ssl on;/ }
+  its(:content) { should match /ssl_certificate .*default\.pem;/ }
+  its(:content) { should match /ssl_certificate_key .*default\.key;/ }
+end
+
 [
-  file('/srv/http/default/ssl'),
-  file('/srv/http/default/ssl/keys'),
-  file('/srv/http/default/ssl/certs')
+  file(ssl_dir),
+  file(File.join(ssl_dir, 'keys')),
+  file(File.join(ssl_dir, 'certs'))
 ].each do |path|
   describe path do
     it { should be_directory }
@@ -13,17 +26,18 @@ require 'spec_helper'
 end
 
 [
-  file('/srv/http/default/ssl/keys/default.key'),
-  file('/srv/http/default/ssl/certs/default.pem')
+  file(key_path),
+  file(cert_path)
 ].each do |path|
   describe path do
     it { should be_file }
   end
 end
 
-describe file('/etc/nginx/sites/default.conf') do
-  its(:content) { should match /listen 443;/ }
-  its(:content) { should match /ssl on;/ }
-  its(:content) { should match /ssl_certificate .*default\.pem;/ }
-  its(:content) { should match /ssl_certificate_key .*default\.key;/ }
+describe command("#{subject_command} #{cert_path}") do
+  its(:stdout) { should match /subject= \/CN=default\.example/ }
+end
+
+describe command("#{fingerprint_command} /srv/http/secondary/ssl/certs/secondary.pem") do
+  its(:stdout) { should match /SHA1 Fingerprint=90:35:5E:38:92:8B:5C:4A:75:24:01:14:44:D6:A3:45:DB:00:34:C1/ }
 end
